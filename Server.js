943866c9818dba47a3649d80a30550e3b057df4a
Daniel <danieljbloch95@gmail.com>
2020-01-04 14:48:49 -0500

    Fixed Server

--- Server.js
+++ Server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const sesssion = require('express-session')
-const App = express()
+const session = require('express-session')
+const app = express()
+const db = mongoose.connection
 
-app.use(express.json)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
 app.use(express.static('public'))
 app.use(session({
   secret: 'feedmeseymour',
@@ -11,7 +13,7 @@ app.use(session({
   saveUnitialized: false
 }))
 
-const starwarsControllers = require('./controllers/starwars.js')
+const starwarsController = require('./controllers/starwars.js')
 app.use('/starwars', starwarsController)
 
 const usersController = require('./controllers/users.js')
@@ -20,11 +22,11 @@ app.use('/users', usersController)
 const sessionController = require('./controllers/session.js')
 app.use('/session', sessionController)
 
-mongoose.connect('mongodb://localhost:27017/meancrud,' {useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/meancrud', {useNewUrlParser:true, useUnifiedTopology: true})
 mongoose.connection.once('open', () => {
-  console.log('connected to mongoose');
+  console.log('connected to mongoose')
 })
 
-app.listen(4000, () => {
-  console.log('listening');
+app.listen(3000, () => {
+  console.log('listening')
 })
